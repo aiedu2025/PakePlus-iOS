6c20603f3c5e90fdd48eab67c55c71b1e40af7d3
aiedu2025 <aiedu2025@outlook.com>
2025-07-07 17:18:37 +0800

    update custom js from pakeplus

--- PakePlus/custom.js
+++ PakePlus/custom.js
@@ -1,6 +1,6 @@
 console.log(
-    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
-    'color:orangered;font-weight:bolder'
+    // %cbuild from PakePlus： https://github.com/',
+    // color:orangered;font-weight:bolder'
 )
 
 // very important, if you don't know what it is, don't touch it
@@ -10,17 +10,38 @@ const hookClick = (e) => {
     const isBaseTargetBlank = document.querySelector(
         'head base[target="_blank"]'
     )
-    console.log('origin', origin, isBaseTargetBlank)
+    // console.log('origin', origin, isBaseTargetBlank)
     if (
         (origin && origin.href && origin.target === '_blank') ||
         (origin && origin.href && isBaseTargetBlank)
     ) {
         e.preventDefault()
-        console.log('handle origin', origin)
+        // console.log('handle origin', origin)
         location.href = origin.href
     } else {
-        console.log('not handle origin', origin)
+        // console.log('not handle origin', origin)
     }
 }
 
 document.addEventListener('click', hookClick, { capture: true })
+
+const dy = (e) => {
+  var url;
+  url = window.location.href;
+  if (url.includes("login")) {
+   var t_Nd = document.evaluate(".//*[contains(text(), '学习系统')]", document.body, null, XPathResult.ANY_TYPE, null ).iterateNext();
+   if (t_Nd){
+     t_Nd.innerHTML='智适应学习系统';
+     t_Nd.setAttribute('style', 'color: #f28f1b;font-size: 35px;text-align: center;letter-spacing: 0.53571rem;');
+   }
+  }
+  
+  const currentDate = new Date();
+  const targetDate = new Date(2026, 7, 1); 
+  if (currentDate > targetDate) {
+    document.documentElement.style.visibility = 'hidden';
+    // document.documentElement.style.display = 'none';
+  }
+}
+document.addEventListener('readystatechange', dy, { capture: true })
+document.addEventListener('DOMContentLoaded', dy, { capture: true })
